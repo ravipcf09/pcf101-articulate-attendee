f1e2194da0fc5a99d91fc55ab13b7b9484cf9ad8
ravipcf09 <52192480+ravipcf09@users.noreply.github.com>
2019-07-09 14:19:46 +0530

    added more comments

--- articulate/src/main/java/io/pivotal/education/articulate/controller/ArticulateRestController.java
+++ articulate/src/main/java/io/pivotal/education/articulate/controller/ArticulateRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class ArticulateRestController {
-
+//enviromentalhelper
 	private final EnvironmentHelper environmentHelper;
 
 	public ArticulateRestController(EnvironmentHelper environmentHelper) {
